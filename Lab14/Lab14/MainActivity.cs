5575dbe720f0528a5a3ae9b656ce38d475f2b9c2
Alejandro Aguilar <r.alejandro.aguilar.m@gmail.com>
2017-07-01 12:50:03 -0700

    Lab14: Final

--- Lab14/Lab14/MainActivity.cs
+++ Lab14/Lab14/MainActivity.cs
@@ -1,6 +1,7 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using System.Threading.Tasks;
 
 namespace Lab14
 {
@@ -13,6 +14,29 @@ namespace Lab14
 
             // Set our view from the "main" layout resource
             SetContentView (Resource.Layout.Main);
+            var ButtonImage = FindViewById<Button>(Resource.Id.buttonValidate);
+
+            ButtonImage.Click += async (s, e) =>
+            {
+                await Validar();
+            };
+
+        }
+
+        async Task Validar()
+        {
+            var Client = new SALLab14.ServiceClient();
+            string Email = "r.alejandro.aguilar.m@gmail.com";
+            string Pass = "nemesis@23";
+            var Result = await Client.ValidateAsync(this);
+
+            Android.App.AlertDialog.Builder Builder = new AlertDialog.Builder(this);
+            AlertDialog Alert = Builder.Create();
+            Alert.SetTitle("Resultado de la verificación");
+            Alert.SetIcon(Resource.Drawable.Icon);
+            Alert.SetMessage($"{Result.Status}\n{Result.FullName}\n{Result.Token}");
+            Alert.SetButton("OK", (s, e) => { });
+            Alert.Show();
         }
     }
 }
